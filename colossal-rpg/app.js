b79c5ebe6ed79dfab08dc3bcce8db049e524f507
Ashley Stanley <astanley006@gmail.com>
2020-05-13 15:41:26 -0400

    Add files via upload

--- colossal-rpg/app.js
+++ colossal-rpg/app.js
@@ -4,26 +4,35 @@ const readline = require('readline-sync');
 
 console.log("Hello!");
 const whatName = readline.question('What is your name? ');
-const firstName = whatName.valueOf().toUpperCase();
+const firstName = whatName.toUpperCase();
 
 // Type play to start
 const play = readline.question("Hello " + firstName + " Are you ready? Let's play! Type 'play' to continue.");
+    if (play === "play") {
+        console.log("You have just woken up in a hotel where a murderer is on the loose... (for example)")
+         
+        // Here will go the beginning of the game story. (Type 's' to skip)
+    }
 
-// function gameStart() {
-//     if (play.valueOf() === "play") {
-//         return commandKey + isPlaying;
-//     } 
-// }
 
+const skip = readline.keyIn("Type 'S' to SKIP")
+
+skipHandler(skip);
+
+function skipHandler() {
+    if (skip === "s") {
+        console.log("Game started!")
+    }
+}
 
 // function commandOptions() {
 
 //     for (let i = 0; i < defaultOptions.length; i++) {
-//         if (currentDefaultOptions.valueOf() === "r") {
+//         if (currentDefaultOptions === "r") {
 //             console.log("You are running away!")
-//         } else if (currentDefaultOptions.valueOf() === "a") {
+//         } else if (currentDefaultOptions === "a") {
 //             console.log("Attack Enemy!")
-//         } else if (currentDefaultOptions.valueOf() === "j") {
+//         } else if (currentDefaultOptions === "j") {
 //             console.log("Jump command!")
 //         }
          
@@ -34,47 +43,15 @@ const play = readline.question("Hello " + firstName + " Are you ready? Let's pla
 
 
 
-let commandKey = readline.keyIn("hi. Type c for 'commands' if you get stuck " )
-if (commandKey.valueOf() === "c") {
-    let commands = readline.keyIn("COMMANDS:" + ["'WALK' = 'W', 'RUN' = 'R', 'ATTACK' = 'A', 'JUMP' = 'J' 'HELP' = 'H'"])
-}
+let commandKey = readline.keyIn("Type c for 'commands' if you get stuck " )
+    if (commandKey === "c") {
+        let commands = readline.keyIn("COMMANDS:" + ["'WALK' = 'W', 'RUN' = 'R', 'ATTACK' = 'A', 'JUMP' = 'J' 'HELP' = 'H', 'SKIP' = 'S'"])
+        skipHandler(commands)
+    }
 
 
-const defaultOptions = ["W", "R", "A", "J", "H", "C"];
+const defaultOptions = ["W", "R", "A", "J", "H", "C", "S"];
 const currentDefaultOptions = readline.setDefaultOptions({limit: defaultOptions})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let runKey = command.keyIn("")
-// if (runKey.valueOf() === "r") {
-//     return "You are running";
-// }
-
-
-// function gameOn() {
-//     while (isPlaying) {
-//         // Player is not dead
-//     }
-// }
-
-
-
-
 
 
 
@@ -82,7 +59,7 @@ const currentDefaultOptions = readline.setDefaultOptions({limit: defaultOptions}
 // Type "help" to understand premise of the game
 
 // function helpKey() {
-//     if (commands.valueOf() === "h") {
+//     if (commands === "h") {
 //         console.log("This is some help")
 //     }
 // }
@@ -92,60 +69,85 @@ const currentDefaultOptions = readline.setDefaultOptions({limit: defaultOptions}
 // Every time the player walks, a random algorithm will be run that determines 
 // if a wild enemy has appeared (A 1/3 or 1/4 chance of being attacked)
 
-const walkCommand = readline.question('Please enter the letter "w" to walk');
-    let isWalking = true;
+let gameRunning = true;
+let inBattle = false;
+
+let walkCommand = readline.keyIn('Please enter the letter "W" to WALK')    
+    walkHandler(walkCommand)
 
 
-function walk() {
-    if (walkCommand.valueOf() === "w") {
-        console.log("Now walking forward... ")
-        console.log("An enemy is approaching!")   
-        
-    } 
+function walkHandler() {
+    if(walkCommand === "w") {
+        console.log("Walking... How tf do we get out of here?")
+    }
 }
-    
-    
 
+// Enemy Appears
+
+const enemyAhead = readline.keyIn("Be careful! There's an enemy ahead! Type 'R' to 'RUN' or 'H' to 'HIDE");
+
+
+    enemyAheadHandler(enemyAhead)
+
+function enemyAheadHandler() {
+    if(enemyAhead === "r") {
+        console.log("Hurry! He's much faster than you!")
+    } else if(enemyAhead === "h") {
+        console.log("Good job hiding from the enemy")
+    }
+}
 
 const enemies = ["Staff", "Mad Dr", "Deranged Patient"]
+    enemyCreation(enemies)
 
-function enemyCreation() {
-    while(isWalking) {
-        
-           
-        for (let i = 0; i < enemies.length; i++) {
-            console.log(enemies[i]); 
-    //I really just guessed here. I have no idea how to generate an algorithm that just choses an enemy at random
-        }
-    }
+// Enemy Alerted
 
-function enemyAttack() {
-
-    if (enemyAlert.valueOf() === "a") {
-        return "ENEMY ATTACK";
-    }
-    else if (enemyAlert.valueOf() === "r") {
-        return "RUNNING AWAY";
-    }
-    
-}
-
-    let enemyAlert = readline.keyIn("Enemy Alerted! Type 'A' to ATTACK. Type 'R' to RUN")
-    enemyAlert.enemyAttack();
-}
-
-
-// If a wild enemy appears:
-
-
-
-
-
-
+const enemyAlert = readline.keyIn(enemies + "alerted! Type 'R' to 'RUN' or 'A' to 'ATTACK'") 
+    enemyAlertHandler(enemyAlert)
 
 // The user can decide to attack or run
 // If attacking, a random amount of damage will be delt between a min and max
+
 // If running, there will be a 50% chance of escaping
+
+
+
+function enemyAlertHandler() {
+    if(enemyAlert === "r") {
+        console.log("You're running away!")
+        
+    } else if(enemyAlert === "a") {
+        console.log("Enemy hit!")
+    }
+}
+
+
+function enemyAttack() {
+
+    enemyAlertHandler(enemyAlert)
+    
+}
+
+function enemyCreation() {
+    for(var i = 0; i < enemies.length; i++) {
+        Math.random(enemies[i])
+    } if (inBattle) {
+        enemyAheadHandler(enemyAlert)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 // After the player attacks or runs the enemy attacks back for a random damage amount
 // If the player kills the enemy you can give the Player some HP and a special item that is stored in the inventory. After this, the player will continue walking. Use a while loop to control this flow.
 // If the enemy kills the player the console prints a cool death message and the game ends
